15837c1d609ae41439c0e24990f1b3c037b8778c
almubadev <soy@almuba.dev>
2025-02-24 15:40:17 -0300

    Fix static files configuration

--- config/settings.py
+++ config/settings.py
@@ -124,9 +124,6 @@ CORS_ALLOW_METHODS = [
 # Configuración de archivos estáticos y media
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/mCEEE/mCEEE/static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/home/mCEEE/mCEEE/media'  # Ruta en PythonAnywhere
